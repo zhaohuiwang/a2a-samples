966f07342b9410d369f6b13b9116f8ea68a37c86
Mathew Shen <datahonor@gmail.com>
2025-04-11 06:54:23 +0800

    fix: get_agent_card return type (#44)

--- demo/ui/utils/agent_card.py
+++ demo/ui/utils/agent_card.py
@@ -1,7 +1,7 @@
 import requests
 from common.types import AgentCard
 
-def get_agent_card(remote_agent_address: str) -> str:
+def get_agent_card(remote_agent_address: str) -> AgentCard:
   """Get the agent card."""
   agent_card = requests.get(
       f"http://{remote_agent_address}/.well-known/agent.json"
