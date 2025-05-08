a52666b353461af68a4d98f298b85241d83dab5c
Mike Smith <mikeas@google.com>
2025-05-08 17:03:04 +0000

    Create an example showing an ADK agent using an authenticated tool.
    b/415795340
    
    Change-Id: I4fd36923669be6343292aa7173805b7a86dcbabb

---
+++ examples/google_adk/calendar_agent/__main__.py
@@ -0,0 +1,106 @@
+import logging
+import os
+import sys
+
+import click
+import uvicorn
+
+from adk_agent_executor import ADKAgentExecutor
+from adk_agent import create_agent
+from dotenv import load_dotenv
+
+from a2a.server.tasks import InMemoryTaskStore
+from a2a.server.apps import A2AStarletteApplication
+from a2a.server.request_handlers import DefaultRequestHandler
+from a2a.types import (
+    AgentAuthentication,
+    AgentCapabilities,
+    AgentCard,
+    AgentSkill,
+)
+from google.adk.artifacts import InMemoryArtifactService
+from google.adk.memory.in_memory_memory_service import InMemoryMemoryService
+from google.adk.runners import Runner
+from google.adk.sessions import InMemorySessionService
+from starlette.applications import Starlette
+from starlette.routing import Route
+from starlette.requests import Request
+from starlette.responses import PlainTextResponse
+
+load_dotenv()
+
+logging.basicConfig()
+
+
+@click.command()
+@click.option('--host', 'host', default='localhost')
+@click.option('--port', 'port', default=10007)
+def main(host: str, port: int):
+    # Verify an API key is set. Not required if using Vertex AI APIs, since those can use gcloud credentials.
+    if not os.getenv('GOOGLE_GENAI_USE_VERTEXAI') == 'TRUE':
+        if not os.getenv('GOOGLE_API_KEY'):
+            raise Exception(
+                'GOOGLE_API_KEY environment variable not set and GOOGLE_GENAI_USE_VERTEXAI is not TRUE.'
+            )
+
+    skill = AgentSkill(
+        id='check_availability',
+        name='Check Availability',
+        description="Checks a user's availability for a time using their Google Calendar",
+        tags=['calendar'],
+        examples=['Am I free from 10am to 11am tomorrow?'],
+    )
+
+    agent_card = AgentCard(
+        name='Calendar Agent',
+        description="An agent that can manage a user's calendar",
+        url=f'http://{host}:{port}/',
+        version='1.0.0',
+        defaultInputModes=['text'],
+        defaultOutputModes=['text'],
+        capabilities=AgentCapabilities(streaming=True),
+        skills=[skill],
+        authentication=AgentAuthentication(schemes=['public']),
+    )
+
+    adk_agent = create_agent(
+        client_id=os.getenv('GOOGLE_CLIENT_ID'),
+        client_secret=os.getenv('GOOGLE_CLIENT_SECRET'),
+    )
+    runner = Runner(
+        app_name=agent_card.name,
+        agent=adk_agent,
+        artifact_service=InMemoryArtifactService(),
+        session_service=InMemorySessionService(),
+        memory_service=InMemoryMemoryService(),
+    )
+    agent_executor = ADKAgentExecutor(runner, agent_card)
+
+    async def handle_auth(request: Request) -> PlainTextResponse:
+        await agent_executor.on_auth_callback(
+            request.query_params.get('state'), str(request.url)
+        )
+        return PlainTextResponse('Authentication successful.')
+
+    request_handler = DefaultRequestHandler(
+        agent_executor=agent_executor, task_store=InMemoryTaskStore()
+    )
+
+    a2a_app = A2AStarletteApplication(
+        agent_card=agent_card, http_handler=request_handler
+    )
+    routes = a2a_app.routes()
+    routes.append(
+        Route(
+            path='/authenticate',
+            methods=['GET'],
+            endpoint=handle_auth,
+        )
+    )
+    app = Starlette(routes=routes)
+
+    uvicorn.run(app, host=host, port=port)
+
+
+if __name__ == '__main__':
+    main()
