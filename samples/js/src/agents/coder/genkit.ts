fc690806228c316ade7fabceaef1848a6fc30958
Pavel Jbanov <pavelgj@gmail.com>
2025-05-13 11:52:25 -0400

    chore: upgrade genkit to the latest version (1.8.0) (#477)

--- samples/js/src/agents/coder/genkit.ts
+++ samples/js/src/agents/coder/genkit.ts
@@ -1,10 +1,10 @@
 import { genkit } from "genkit/beta";
 import { defineCodeFormat } from "./code-format.js";
-import { gemini20Flash, googleAI } from "@genkit-ai/googleai";
+import { googleAI } from "@genkit-ai/googleai";
 
 export const ai = genkit({
   plugins: [googleAI()],
-  model: gemini20Flash.withConfig({ version: "gemini-2.5-pro-exp-03-25" }),
+  model: googleAI.model("gemini-2.5-pro-exp-03-25"),
 });
 
 defineCodeFormat(ai);
--- samples/js/src/agents/movie-agent/genkit.ts
+++ samples/js/src/agents/movie-agent/genkit.ts
@@ -1,11 +1,11 @@
-import { googleAI, gemini20Flash } from "@genkit-ai/googleai";
+import { googleAI } from "@genkit-ai/googleai";
 import { genkit } from "genkit";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 
 export const ai = genkit({
   plugins: [googleAI()],
-  model: gemini20Flash,
+  model: googleAI.model("gemini-2.0-flash"),
   promptDir: dirname(fileURLToPath(import.meta.url)),
 });
 
