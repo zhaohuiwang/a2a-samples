474f792e0c16ef58c636c352b8ca2269252920ec
Krishna Thota <kcthota@google.com>
2025-05-13 11:49:58 -0700

    Miscellaneous fixes
    Change-Id: Ic545b230c7dd153e105c855b68c43fab4a010fa3

--- examples/helloworld/agent_executor.py
+++ examples/helloworld/agent_executor.py
@@ -1,28 +1,18 @@
-import asyncio
-
-from collections.abc import AsyncGenerator
-from typing import Any
-from uuid import uuid4
-
 from typing_extensions import override
+
 from a2a.server.agent_execution import AgentExecutor, RequestContext
-from a2a.server.events import Event, EventQueue
-from a2a.utils import new_agent_text_message
+from a2a.server.events import EventQueue
 from a2a.types import (
-    Message,
-    Part,
-    Role,
-    SendMessageRequest,
-    SendStreamingMessageRequest,
     Task,
-    TextPart,
 )
+from a2a.utils import new_agent_text_message
+
 
 class HelloWorldAgent:
     """Hello World Agent."""
 
     async def invoke(self) -> str:
-      return 'Hello World'
+        return 'Hello World'
 
 
 class HelloWorldAgentExecutor(AgentExecutor):
@@ -41,5 +31,7 @@ class HelloWorldAgentExecutor(AgentExecutor):
         event_queue.enqueue_event(new_agent_text_message(result))
 
     @override
-    async def cancel(self, request: RequestContext, event_queue: EventQueue) -> Task | None:
-        raise Exception("cancel not supported")
+    async def cancel(
+        self, request: RequestContext, event_queue: EventQueue
+    ) -> Task | None:
+        raise Exception('cancel not supported')
