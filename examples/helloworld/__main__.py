b626f1fa20ebb476f11f4b1c61cb429fe340a2e0
kthota-g <kcthota@google.com>
2025-05-14 22:33:08 -0700

    fix: Fix mypy errors and enable streaming for hw example (#15)

--- examples/helloworld/__main__.py
+++ examples/helloworld/__main__.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
         version='1.0.0',
         defaultInputModes=['text'],
         defaultOutputModes=['text'],
-        capabilities=AgentCapabilities(),
+        capabilities=AgentCapabilities(streaming=True),
         skills=[skill],
         authentication=AgentAuthentication(schemes=['public']),
     )
