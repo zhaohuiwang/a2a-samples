ef7fc1278735509cddc4fb3c444f494db4fb8a1b
Rajesh Velicheti <rvelicheti@google.com>
2025-06-02 22:26:23 +0530

    feat: A2A Tracing: Demonstrate tracing in A2A SDK via a QnA Agent with Jaeger and Grafana (#85)
    * Demonstrate tracing in A2A SDK via a QnA Agent with Jaeger and Grafana
    
    * Fix: add a terminal status when the agent does not have a final response

---
+++ samples/python/agents/a2a_telemetry/__main__.py
@@ -0,0 +1,88 @@
+import logging
+import os
+
+import click
+import uvicorn
+
+from a2a.server.apps import A2AStarletteApplication
+from a2a.server.request_handlers import DefaultRequestHandler
+from a2a.server.tasks import InMemoryTaskStore
+from a2a.types import AgentCapabilities, AgentCard, AgentSkill
+from agent_executor import QnAAgentExecutor
+from opentelemetry import trace
+from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import (
+    OTLPSpanExporter,
+)
+from opentelemetry.instrumentation.starlette import StarletteInstrumentor
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+
+logger = logging.getLogger(__name__)
+logging.basicConfig()
+
+
+@click.command()
+@click.option('--host', 'host', default='localhost')
+@click.option('--port', 'port', default=10020)
+def main(host: str, port: int):
+    """A2A Telemetry Sample GRPC Server."""
+    if not os.getenv('GOOGLE_API_KEY'):
+        raise ValueError('GOOGLE_API_KEY is not set.')
+
+    skill = AgentSkill(
+        id='question_answer',
+        name='Q&A Agent',
+        description='A helpful assistant agent that can answer questions.',
+        tags=['Question-Answer'],
+        examples=[
+            'Who is leading 2025 F1 Standings?',
+            'Where can i find an active volcano?',
+        ],
+    )
+
+    agent_executor = QnAAgentExecutor()
+    agent_card = AgentCard(
+        name='Q&A Agent',
+        description='A helpful assistant agent that can answer questions.',
+        url=f'http://{host}:{port}/',
+        version='1.0.0',
+        defaultInputModes=['text'],
+        defaultOutputModes=['text'],
+        capabilities=AgentCapabilities(streaming=True),
+        skills=[skill],
+    )
+    request_handler = DefaultRequestHandler(
+        agent_executor=agent_executor, task_store=InMemoryTaskStore()
+    )
+
+    logger.debug('Telemetry Configuration')
+    # Set the service name for query back in Jaeger and Grafana
+    resource = Resource(
+        attributes={
+            'service.name': 'a2a-telemetry-sample',
+            'service.version': '1.0',
+        }
+    )
+    # Create a TracerProvider and register it
+    tracer_provider = TracerProvider(resource=resource)
+    trace.set_tracer_provider(tracer_provider)
+    tracer_provider = trace.get_tracer_provider()
+
+    # Create and configure Jaeger exporter, UDP transport.
+    jaeger_exporter = OTLPSpanExporter(
+        endpoint='http://localhost:4317', insecure=True
+    )
+    tracer_provider.add_span_processor(BatchSpanProcessor(jaeger_exporter))
+
+    server = A2AStarletteApplication(agent_card, request_handler)
+    starlette_app = server.build()
+    # Instrument the starlette app for tracing
+    StarletteInstrumentor().instrument_app(starlette_app)
+    uvicorn.run(starlette_app, host=host, port=port)
+
+
+if __name__ == '__main__':
+    main()
---
+++ samples/python/agents/a2a_telemetry/agent_executor.py
@@ -0,0 +1,100 @@
+import logging
+
+from a2a.server.agent_execution import AgentExecutor, RequestContext
+from a2a.server.events import EventQueue
+from a2a.server.tasks import TaskUpdater
+from a2a.types import TaskState, TextPart, UnsupportedOperationError
+from a2a.utils.errors import ServerError
+from google.adk import Runner
+from google.adk.agents import LlmAgent
+from google.adk.artifacts import InMemoryArtifactService
+from google.adk.memory.in_memory_memory_service import InMemoryMemoryService
+from google.adk.sessions import InMemorySessionService
+from google.adk.tools import google_search_tool
+from google.genai import types
+
+
+logger = logging.getLogger(__name__)
+
+
+class QnAAgentExecutor(AgentExecutor):
+    """Agent executor that uses the ADK to answer questions."""
+
+    def __init__(self):
+        self.agent = None
+        self.runner = None
+
+    def _init_agent(self):
+        self.agent = LlmAgent(
+            model='gemini-2.0-flash-exp',
+            name='question_answer_agent',
+            description='A helpful assistant agent that can answer questions.',
+            instruction="""Respond to the query using google search""",
+            tools=[google_search_tool.google_search],
+        )
+        self.runner = self.runner = Runner(
+            app_name=self.agent.name,
+            agent=self.agent,
+            artifact_service=InMemoryArtifactService(),
+            session_service=InMemorySessionService(),
+            memory_service=InMemoryMemoryService(),
+        )
+
+    async def cancel(self, context: RequestContext, event_queue: EventQueue):
+        raise ServerError(error=UnsupportedOperationError())
+
+    async def execute(
+        self,
+        context: RequestContext,
+        event_queue: EventQueue,
+    ) -> None:
+        if self.agent is None:
+            self._init_agent()
+        logger.debug(f'Executing agent {self.agent.name}')
+
+        query = context.get_user_input()
+
+        updater = TaskUpdater(event_queue, context.task_id, context.context_id)
+
+        if not context.current_task:
+            updater.submit()
+
+        updater.start_work()
+
+        content = types.Content(role='user', parts=[types.Part(text=query)])
+        session = await self.runner.session_service.get_session(
+            app_name=self.runner.app_name,
+            user_id='123',
+            session_id=context.context_id,
+        ) or await self.runner.session_service.create_session(
+            app_name=self.runner.app_name,
+            user_id='123',
+            session_id=context.context_id,
+        )
+
+        async for event in self.runner.run_async(
+            session_id=session.id, user_id='123', new_message=content
+        ):
+            logger.debug(f'Event from ADK {event}')
+            if event.is_final_response():
+                parts = event.content.parts
+                text_parts = [
+                    TextPart(text=part.text) for part in parts if part.text
+                ]
+                updater.add_artifact(
+                    text_parts,
+                    name='result',
+                )
+                updater.complete()
+                break
+            else:
+                updater.update_status(
+                    TaskState.working,
+                    message=updater.new_text_message('Working...'),
+                )
+        else:
+            logger.debug('Agent failed to complete')
+            updater.update_status(
+                TaskState.failed,
+                message=updater.new_text_message('Failed to generate a response.'),
+            )
