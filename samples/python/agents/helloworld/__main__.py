325e032599acbc34dd941511566e3bc13a8d43c5
Iizalaarab Elhaimeur <106118624+Izzy-Elizzy@users.noreply.github.com>
2025-06-18 11:25:50 -0400

    The public agent card URL for the helloworld server/agent is http://0.0.0.0:9999/, which is a server-side configuration and causes a 503 error when the client attempts to connect to it. This error was fixed by changing public agent card URL for the HelloWorld Agent to http://localhost:9999/. Test Client for HelloWorld Agent now works as intended. (#130)

--- samples/python/agents/helloworld/__main__.py
+++ samples/python/agents/helloworld/__main__.py
@@ -37,7 +37,7 @@ if __name__ == '__main__':
     public_agent_card = AgentCard(
         name='Hello World Agent',
         description='Just a hello world agent',
-        url='http://0.0.0.0:9999/',
+        url='http://localhost:9999/',
         version='1.0.0',
         defaultInputModes=['text'],
         defaultOutputModes=['text'],
